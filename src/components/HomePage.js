7a3e6777f006ef8d2f1671111e0cb063b0880512
mohdmazharuddin <106807606+mohdmazharuddin@users.noreply.github.com>
2023-08-01 12:57:49 +0530

    introduction of axios

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,29 +1,107 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const HomePage = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  // {
+  //   title: 'foo',
+  //   body: 'bar',
+  //   userId: 1,
+  // }
+
   const onClick = () => {
     setIsLoading(true);
-    fetch("https://dummyjson.com/products")
-      .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setData(json);
-        setIsLoading(false);
-        console.log(json);
-      });
+
+    // axios
+    //   .get("https://jsonplaceholder.typicode.com/posts")
+    //   .then((responce) => {
+    //     setData(responce.data);
+    //     setIsLoading(false);
+    //   })
+    //   .catch();
+
+    // fetch("https://dummyjson.com/products")
+    //   .then((res) => {
+    //     return res.json();
+    //   })
+    //   .then((json) => {
+    //     setData(json);
+    //     setIsLoading(false);
+    //     console.log(json);
+    //   });
+
+    /// post call
+
+    // fetch("https://jsonplaceholder.typicode.com/posts", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     title: "foo",
+    //     body: "bar",
+    //     userId: 1,
+    //   }),
+    //   headers: {
+    //     "Content-type": "application/json; charset=UTF-8",
+    //   },
+    // })
+    //   .then((res) => res.json())
+    //   .then((json) => console.log(json))
+    //   .catch((error) => {
+    //     alert(error);
+    //   });
+
+    // axios
+    //   .post(
+    //     "https://jsonplaceholder.typicode.com/posts",
+    //     {
+    //       title: "tekiksy",
+    //       body: "software company",
+    //       userId: 1,
+    //     }
+    //   )
+    //   .then((responce) => console.log(responce.data))
+    //   .catch();
+
+    // put call
+
+    // fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    //   method: "PUT",
+    //   body: JSON.stringify({
+    //     id: 1,
+    //     title: "foo",
+    //     body: "bar",
+    //     userId: 1,
+    //   }),
+    //   headers: {
+    //     "Content-type": "application/json; charset=UTF-8",
+    //   },
+    // }).then().catch()
+
+    // axios.put("https://jsonplaceholder.typicode.com/posts/1", {
+    //   id: 1,
+    //   title: "foo",
+    //   body: "bar",
+    //   userId: 1,
+    // });
+
+    // delete call
+
+    // fetch("https://jsonplaceholder.typicode.com/posts/1", { method: "DELETE" })
+    //   .then()
+    //   .catch();
+
+    axios.delete("https://jsonplaceholder.typicode.com/posts/1");
   };
+
   return (
     <div>
       {data.length === 0 && <button onClick={onClick}>get data</button>}
       {isLoading === true ? (
         <h1>Loading....</h1>
       ) : (
-        data?.products?.map((value, index) => {
+        data?.map((value, index) => {
           return (
             <p key={value.id}>
               <Link to={`/productDetail/${value.id}`}>
